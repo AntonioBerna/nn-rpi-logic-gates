fdcd0a5b259d578e2d4c32d9cbe83a4486ec37de
AntonioBerna <asukeasukai12@gmail.com>
2023-06-26 02:41:02 +0200

    :memo:

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # nn-rpi-logic-gates
 Logic Gates LED Control using Raspberry Pi and Neural Networks: A project that demonstrates the use of a Raspberry Pi to control LEDs based on logic gates, utilizing a neural network trained with backpropagation.
 
+# Da sistemare...
+L'algoritmo di Machine Learning utilizzato in questo caso è un tipo di rete neurale chiamato "Multilayer Perceptron" (MLP). L'MLP è una rete neurale feedforward che consiste in almeno tre strati: uno strato di input, uno o più strati nascosti e uno strato di output.
+
+Nel codice fornito, la classe NeuralNetwork implementa un MLP con un singolo strato nascosto. Viene utilizzata la funzione di attivazione sigmoid per calcolare l'output dei neuroni in ciascuno strato, e l'algoritmo di retropropagazione dell'errore per addestrare la rete aggiornando i pesi in base all'errore calcolato tra l'output atteso e l'output effettivo della rete.
+
+L'MLP con retropropagazione dell'errore è uno degli algoritmi più comuni per l'apprendimento supervisionato e viene utilizzato per problemi di classificazione e regressione.
+
+
+Link Utili:
+- https://www.edutecnica.it/sistemi/boole/boole.htm
+- https://www.raspberrypi.com/documentation/computers/os.html
+- 
---
+++ mlp/nn.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class NeuralNetwork:
+    def __init__(self, input_size, hidden_size, output_size):
+        # Inizializzazione dei pesi con valori casuali
+        self.weights_input_hidden = np.random.uniform(size=(input_size, hidden_size))
+        self.weights_hidden_output = np.random.uniform(size=(hidden_size, output_size))
+
+    def feedforward(self, inputs):
+        # Calcolo dell'output della rete neurale
+        hidden_layer_output = sigmoid(np.dot(inputs, self.weights_input_hidden))
+        output = sigmoid(np.dot(hidden_layer_output, self.weights_hidden_output))
+        return output
+
+    def train(self, inputs, expected_output, learning_rate):
+        # Feedforward
+        hidden_layer_output = sigmoid(np.dot(inputs, self.weights_input_hidden))
+        output = sigmoid(np.dot(hidden_layer_output, self.weights_hidden_output))
+
+        # Calcolo dell'errore
+        error = expected_output - output
+
+        # Retropropagazione dell'errore
+        output_delta = error * sigmoid_derivative(output)
+        hidden_layer_error = np.dot(output_delta, self.weights_hidden_output.T)
+        hidden_layer_delta = hidden_layer_error * sigmoid_derivative(hidden_layer_output)
+
+        # Aggiornamento dei pesi
+        self.weights_hidden_output += learning_rate * np.outer(hidden_layer_output, output_delta)
+        self.weights_input_hidden += learning_rate * np.outer(inputs, hidden_layer_delta)
+
