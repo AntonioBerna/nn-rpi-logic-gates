73650eaf8200b0fb38620ddc4a9424e0efed2ff8
AntonioBerna <antoniobernardini@live.com>
2024-08-03 17:59:35 +0200

    edited output style of code :fire:

--- src/main.py
+++ src/main.py
@@ -21,19 +21,20 @@ if __name__ == "__main__":
                         inputs = np.array(features["input"])
                         expected_output = np.array(features["output"])
                         neural_network.train(inputs, expected_output, learning_rate=0.1)
-        
+
                 outputs = []
                 for features in model:
                     inputs = np.array(features["input"])
                     output = neural_network.feedforward(inputs)[0]
                     print(f"{inputs} -> {output}")
                     outputs.append(round(output))
-    
+
                 for output, pin in zip(outputs, led_pins):
                     GPIO.output(pin, GPIO.HIGH if output else GPIO.LOW)
-            
+
                 time.sleep(5)
                 GPIO.output(led_pins, GPIO.LOW)
+                print()
     except KeyboardInterrupt:
         print("Roger That. Exiting.")
     finally:
